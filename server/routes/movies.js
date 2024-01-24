00c7678843f793c1a359f96cfb83547ec2fe8296
Gourav Dey <gourav.dey.ece26@heritageit.edu.in>
2024-01-25 00:36:21 +0530

    Created Movie Route and config file

---
+++ server/routes/movies.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const Movie = require("../models/Movie");
+
+router.get("/movies", async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) - 1 || 0;
+    const limit = parseInt(req.query.limit) || 5;
+    const search = req.query.search || "";
+    let sort = req.query.sort || "rating";
+    let genre = req.query.genre || "All";
+
+    const genreOptions = [
+      "Action",
+      "Adventure",
+      "Animation",
+      "Biography",
+      "Comedy",
+      "Crime",
+      "Documentary",
+      "Drama",
+      "Family",
+      "Fantasy",
+    ];
+
+    genre = genre === "All" ? [...genreOptions] : [genre];
+    sort = req.query.sort ? req.query.sort.split(",") : [sort];
+
+    let sortBy = {};
+    if (sort[1]) {
+      sortBy[sort[0]] = sort[1];
+    } else {
+      sortBy[sort[0]] = "asc";
+    }
+    
+    const movies = await Movie.find({
+      name: { $regex: search, $options: "i" },
+    })
+      .where("genre")
+      .in([...genre])
+      .sort(sortBy)
+      .skip(page * limit)
+      .limit(limit);
+
+    const response = {
+      error: false,
+      total,
+      page: page+1,
+      limit,
+      genres: genreOptions,
+      movies,
+    };
+
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: true, message: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ dbConnect();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", movieRoutes);
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
